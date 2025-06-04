a5e311a5d413e9581510aeb27a86f46913bd6945
Jakub <dsw56154@student.dsw.edu.pl>
2025-06-04 15:41:45 +0200

    logs skrypt v2.0

--- skrypt.sh
+++ skrypt.sh
@@ -3,8 +3,12 @@ SCRIPT_NAME=$(basename "$0")
 if [ "$1" == "--date" ]; then
   date
 elif [ "$1" == "--logs" ]; then
+  num=100
+  if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+    num=$2
+  fi
   for i in $(seq 1 100); do
-    filename="log${i}.txt"
+  filename="log${i}.txt"
     echo "Nazwa pliku: ${filename}" > "$filename"
     echo "Nazwa skryptu: ${SCRIPT_NAME}" >> "$filename"
     echo "Data utworzenia: $(date)" >> "$filename"
