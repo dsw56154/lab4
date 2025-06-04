e2d07d3f80c8cf5a7ad77f8714a68ccb50c663f5
Jakub <dsw56154@student.dsw.edu.pl>
2025-06-04 16:20:07 +0200

    error skrypt

--- skrypt.sh
+++ skrypt.sh
@@ -28,11 +28,19 @@ elif [ "$1" == "--logs" ]; then
   if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
     num=$2
   fi
-  for i in $(seq 1 100); do
+  for i in $(seq 1 "$num"); do
   filename="log${i}.txt"
     echo "Nazwa pliku: ${filename}" > "$filename"
     echo "Nazwa skryptu: ${SCRIPT_NAME}" >> "$filename"
     echo "Data utworzenia: $(date)" >> "$filename"
     echo "Utworzono plik: ${filename}"
+  done
+elif [ "$1" == "--error" ]; then
+  num=100
+  if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+    num=$2
+  fi
+  for i in $(seq 1 "$num"); do
+  filename="error${i}.txt"
   done
 fi
